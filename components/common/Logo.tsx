10ef163940eb5f06ef21c5d104fd35d30501c838
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-10-25 13:10:13 +0600

    feat: complete navbar and reuseable button

---
+++ components/common/Logo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import logo from "/public/images/logo.png";
+import Link from "next/link";
+
+export default function Logo() {
+	return (
+		<Link href="/" className="flex items-center">
+			<Image src={logo} alt="Easy Pay" height={73} className="mt-4 -mr-3" />
+			<h4 className="font-bold text-[28px]">Easy Pay</h4>
+		</Link>
+	);
+}
--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -1,5 +1,34 @@
+import Link from "next/link";
+import Logo from "./Logo";
+import { Button } from "../ui/button";
+
+const navigationLinks = [
+	{ name: "Features", href: "#features" },
+	{ name: "Pricing", href: "#pricing" },
+	{ name: "Security", href: "#security" },
+	{ name: "Benefits", href: "#benefits" },
+];
+
 export default function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+	const renderNavLinks = () => {
+		return navigationLinks.map((link) => (
+			<li key={link.name}>
+				<Link
+					href={link.href}
+					className="font-semibold text-theme-gray text-lg"
+				>
+					{link.name}
+				</Link>
+			</li>
+		));
+	};
+	return (
+		<nav className="flex items-center justify-between py-6 max-w-[1170px] mx-auto h-[121px]">
+			<Logo />
+			<menu className="flex space-x-8">{renderNavLinks()}</menu>
+			<div>
+				<Button>Contact Us</Button>
+			</div>
+		</nav>
+	);
 }
