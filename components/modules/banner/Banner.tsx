24cc3cad63693815030a098b2159ec485e891b74
hazzaz-am <hazzazabdulmannan@gmail.com>
2025-10-27 00:22:45 +0600

    refactor(banner): make banner responsive for all devices

--- components/modules/banner/Banner.tsx
+++ components/modules/banner/Banner.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-
 import appStore from "/public/images/hero/appStore.png";
 import playStore from "/public/images/hero/playStore.png";
 import bannerProfile from "/public/images/hero/bannerImg.png";
@@ -17,27 +16,27 @@ export default function Banner() {
 			}}
 			className="m-6 rounded-3xl mb-[120px]"
 		>
-			<div className="max-w-[1170px] mx-auto">
+			<div className="max-w-[1170px] mx-auto px-4">
 				<Navbar />
-				<div className="flex-col-reverse justify-between lg:flex-row">
-					<div className="pt-20 w-full lg:w-1/2">
+				<div className="flex flex-col-reverse md:flex-col-reverse lg:justify-between lg:flex-row xl:relative lg:items-center">
+					<div className="pb-10 pt-20 w-full lg:max-w-[638px] lg:h-[580px]">
 						<div className="max-w-[638px]">
-							<h4 className="text-base font-semibold text-theme-text uppercase">
+							<h4 className="text-sm lg:text-base font-semibold text-theme-text uppercase">
 								EasyPayment
 							</h4>
-							<h1 className="font-bold text-[64px] text-theme-dark leading-[120%]">
+							<h1 className="font-bold text-2xl md:text-4xl lg:text-5xl xl:text-[64px] text-theme-dark leading-[120%]">
 								Pay fast and smarter
 							</h1>
-							<Image src={underline} alt="Underline" className="ml-auto" />
-							<h1 className="font-bold text-[64px] text-theme-dark leading-[120%]">
+							<Image src={underline} alt="Underline" className="xl:ml-auto" />
+							<h1 className="font-bold text-2xl md:text-4xl lg:text-5xl xl:text-[64px] text-theme-dark leading-[120%]">
 								from anywhere
 							</h1>
-							<p className="text-lg text-theme-gray">
+							<p className="text-base lg:text-lg text-theme-gray font-normal leading-[150%]">
 								Experience the future of payments: fast, secure, and tailored
 								for the next generation&apos;s convenience and trust.
 							</p>
 						</div>
-						<div className="flex gap-2 mt-12">
+						<div className="flex flex-col sm:flex-row gap-2 mt-12">
 							<Image
 								src={appStore}
 								alt="Apple Store"
@@ -50,7 +49,8 @@ export default function Banner() {
 							/>
 						</div>
 					</div>
-					<div className="w-full lg:w-1/2">
+
+					<div className="xl:absolute xl:bottom-0 xl:right-0 w-full xl:max-w-[643px] mx-auto xl:mt-auto">
 						<Image src={bannerProfile} alt="Men" />
 					</div>
 				</div>
